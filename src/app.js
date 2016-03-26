d111f46c770e2a1bcf81cf8361163c116ed085c9
Anthony Lapenna <lapenna.anthony@gmail.com>
2016-03-27 12:34:07 +1300

    Bootstrap css is now bundled with webpack.

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+import 'bootstrap/dist/css/bootstrap.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router, Route, hashHistory, Redirect} from 'react-router';
--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,21 @@ module.exports = {
       loaders: ['react-hot', 'babel?presets[]=react,presets[]=es2015'],
       include: path.join(__dirname, 'src'),
       exclude: /node_modules/
+    }, {
+      test: /\.css$/,
+      loader: "style-loader!css-loader"
+    }, {
+      test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+      loader: 'url?limit=10000&mimetype=application/font-woff'
+    }, {
+      test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+      loader: 'url?limit=10000&mimetype=application/octet-stream'
+    }, {
+      test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+      loader: 'file'
+    }, {
+      test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+      loader: 'url?limit=10000&mimetype=image/svg+xml'
     }]
   },
 
